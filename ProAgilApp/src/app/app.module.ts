b47044c306425d38ce8b1564d4859a126da29411
Fabio Mattes <fabiomattes2007@gmail.com>
2021-04-07 12:56:58 -0300

    Filtros no Angular

--- ProAgilApp/src/app/app.module.ts
+++ ProAgilApp/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, EventosComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
